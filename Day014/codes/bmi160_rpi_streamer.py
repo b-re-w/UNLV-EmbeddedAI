d473144f5fa354f35eb71647af98e5b4669b1f56
venki666 <vm@unlv.nevada.edu>
2026-02-23 10:50:40 -0800

    Add files via upload

--- Day014/codes/bmi160_rpi_streamer.py
+++ Day014/codes/bmi160_rpi_streamer.py
@@ -1,19 +1,20 @@
 import socket
 import time
 import json
-from bmi160 import BMI160
+from BMI160_i2c import Driver
 import smbus2
 
 # --- CONFIGURATION ---
-HOST_IP = '192.168.1.100'  # CHANGE TO HOST PC IP
+HOST_IP = '192.168.1.136'  # CHANGE TO HOST PC IP
 PORT = 65432
+
 
 
 def main():
     # Setup I2C and Sensor
-    i2c = smbus2.SMBus(1)
-    bmi = BMI160(i2c)
-
+    #i2c = smbus2.SMBus(1)
+    #bmi = BMI160(i2c)
+    sensor = Driver(0x69)
     # Connect to Host
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -23,18 +24,21 @@ def main():
 
         while True:
             # Read 6-axis data (returns ax, ay, az, gx, gy, gz)
-            data = bmi.get_motion_6()
+            data = sensor.getMotion6()
 
-            payload = {
-                "ax": data[0],
-                "ay": data[1],
-                "az": data[2],
-                "timestamp": time.time()
-            }
-
+            # payload = {
+            #     "ax": data[0],
+            #     "ay": data[1],
+            #     "az": data[2],
+            #     "timestamp": time.time()
+            # }
+            # Send JSON
+            payload = json.dumps({'ax': data[0], 'ay': data[1], 'az': data[2]}) + "\n"
+            sock.sendall(payload.encode('utf-8'))
+            time.sleep(0.02)  # ~50Hz
             # Send newline-delimited JSON
-            msg = json.dumps(payload) + "\n"
-            sock.sendall(msg.encode('utf-8'))
+            #msg = json.dumps(payload) + "\n"
+            #sock.sendall(msg.encode('utf-8'))
 
             # 50Hz sample rate (fast enough for vibration)
             time.sleep(0.02)
